7fa95e07e71b37ae4e848bdeb024952978518329
Serhey Shmyg <serhey.shmyg@gmail.com>
2019-03-14 16:57:36 +0200

    First commit

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _Test = _interopRequireDefault(require("./Test"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _default = _Test.default;
+exports.default = _default;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6WyJUZXN0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUE7Ozs7ZUFFZUEsYSIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBUZXN0IGZyb20gJy4vVGVzdCc7XG5cbmV4cG9ydCBkZWZhdWx0IFRlc3Q7XG4iXX0=
